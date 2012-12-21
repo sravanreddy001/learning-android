21429dc5d68898b0535423bb85e17a8452a0e668
Sravan <sravanreddy001@gmail.com>
2012-12-20 21:35:45 -0500

    Splash, calling another activity, Playing a sound clip

---
+++ src/com/ganta/learningandroid/Splash.java
@@ -0,0 +1,41 @@
+package com.ganta.learningandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+    MediaPlayer splashSound;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        
+        splashSound = MediaPlayer.create(Splash.this, R.raw.autobots_roll_out);
+        splashSound.start();
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent startActivity = new Intent("android.intent.action.START_SCREEN");
+                    startActivity(startActivity);
+                }
+            }
+        };
+        timer.start();
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        splashSound.release();
+        finish(); // This is activity is killed. (Removed from history in navigation)
+    }
+    
+}
