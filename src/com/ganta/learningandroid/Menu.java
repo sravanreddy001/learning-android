8a9cc7ed895a92a4cf63b89e72a23a04bdf5d803
Sravan <sravanreddy001@gmail.com>
2012-12-22 11:30:30 -0500

    Adding a menu (options) to an activity.

--- src/com/ganta/learningandroid/Menu.java
+++ src/com/ganta/learningandroid/Menu.java
@@ -3,6 +3,7 @@ package com.ganta.learningandroid;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -30,6 +31,15 @@ public class Menu extends ListActivity {
         super.onCreate(savedInstanceState);
         setListAdapter(new ArrayAdapter<String>(Menu.this, android.R.layout.simple_list_item_1, classes));
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater blowUp = getMenuInflater();
+        blowUp.inflate(R.menu.cool_menu, menu);
+        return true;
+    }
+    
     
     
 }
