8b9e0c0ae2a50e6d7b2a8e3890989e4a14a7ecd1
Sravan <sravanreddy001@gmail.com>
2012-12-23 15:41:47 -0500

    1. wake-lock 2. add permissions in Manifest.xml

--- src/com/ganta/learningandroid/GFX.java
+++ src/com/ganta/learningandroid/GFX.java
@@ -1,17 +1,32 @@
 package com.ganta.learningandroid;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 
 public class GFX extends Activity {
 
     MyBringBack ourView;
+    WakeLock wl;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //wake-lock
+        PowerManager pM = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        WakeLock wl = pM.newWakeLock(PowerManager.FULL_WAKE_LOCK, "Tag string");
+        
         super.onCreate(savedInstanceState);
+        wl.acquire(); // Do this in wake onResume() method as well.
         ourView = new MyBringBack(this);
         setContentView(ourView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wl.release();
     }
     
 }
