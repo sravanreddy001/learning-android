ccf45a76fffcf1e74e82ab0c0b346d4477ab9f4d
Sravan <sravanreddy001@gmail.com>
2012-12-23 16:00:15 -0500

    SoundEffects SoundPool, MediaPlayer OnLongClickListener

---
+++ src/com/ganta/learningandroid/SoundStuff.java
@@ -0,0 +1,46 @@
+package com.ganta.learningandroid;
+
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+
+public class SoundStuff extends Activity implements OnClickListener, OnLongClickListener {
+
+    SoundPool sp;
+    int explosion = 0;
+    MediaPlayer mp;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        View v = new View(this);
+        v.setOnClickListener(this);
+        v.setOnLongClickListener(this);
+        setContentView(v);
+        sp = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        explosion = sp.load(this, R.raw.explosion, 1);
+        mp = MediaPlayer.create(this, R.raw.autobots_roll_out);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(explosion != 0) {
+            sp.play(explosion, 1, 1, 0, 0, 1);
+        }
+    }
+
+    @Override
+    public boolean onLongClick(View arg0) {
+        mp.start();
+        return false;
+    }
+    
+    
+    
+}
