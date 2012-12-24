66ca615f1c0dc35a7d80f798d22efe68e4e06789
Sravan <sravanreddy001@gmail.com>
2012-12-24 01:45:42 -0500

    Using SlidingDrawer and trying various methods

--- src/com/ganta/learningandroid/Menu.java
+++ src/com/ganta/learningandroid/Menu.java
@@ -13,7 +13,7 @@ import android.widget.ListView;
 
 public class Menu extends ListActivity {
     String classes[] = { "StartScreenActivity", "TextPlay", "Email", "Camera",
-            "Data", "GFX", "GFXSurface", "SoundStuff" };
+            "Data", "GFX", "GFXSurface", "SoundStuff", "Slider" };
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
