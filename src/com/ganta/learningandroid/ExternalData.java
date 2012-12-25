4772df349449147f14a5672bac79b92408bf8af8
Sravan <sravanreddy001@gmail.com>
2012-12-25 00:43:32 -0500

    Adding a spinner, and ArrayAdapter liked to Spinner

--- src/com/ganta/learningandroid/ExternalData.java
+++ src/com/ganta/learningandroid/ExternalData.java
@@ -3,6 +3,8 @@ package com.ganta.learningandroid;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 /**
@@ -14,6 +16,8 @@ public class ExternalData extends Activity {
     private TextView canWrite, canRead;
     private String state;
     boolean canR, canW;
+    Spinner spinner;
+    String[] paths = {"Music", "Pictures", "Download"};
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,7 @@ public class ExternalData extends Activity {
         setContentView(R.layout.externaldata);
         canRead = (TextView) findViewById(R.id.tvCanRead);
         canWrite = (TextView) findViewById(R.id.tvCanWrite);
+        spinner = (Spinner) findViewById(R.id.spinner1);
         
         state = Environment.getExternalStorageState();
         if(state.equals(Environment.MEDIA_MOUNTED)) {
@@ -40,6 +45,11 @@ public class ExternalData extends Activity {
             canRead.setText("false");
             canR = canW = false;
         }
+        
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ExternalData.this, 
+                android.R.layout.simple_spinner_item, paths);
+        
+        spinner.setAdapter(adapter);
     }
     
 }
